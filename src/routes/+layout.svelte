cd06fa4d27c740bb9d799fc3d15e68823b751077
Raphi <raphael.antonitti@gmail.com>
2025-12-30 00:19:35 +0100

    fix build

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
 	import '../app.css';
+	import { dev } from '$app/environment';
+
+	// Get CSS URL for dev mode workaround
+	const cssUrl = dev ? '/src/app.css' : '';
 </script>
+
+<svelte:head>
+	{#if dev && cssUrl}
+		<link rel="stylesheet" href={cssUrl} />
+	{/if}
+</svelte:head>
 
 <slot />
--- src/routes/[lang]/+layout.svelte
+++ src/routes/[lang]/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import "../../app.css";
   import Header from "$lib/components/Header.svelte";
   import Footer from "$lib/components/Footer.svelte";
   import { siteConfig, getStructuredData, getOrganizationData } from "$lib/seo";
